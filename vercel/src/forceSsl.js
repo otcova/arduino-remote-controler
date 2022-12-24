1ce9b1ec2839a58b0c2dc522ce7abdf37fb641f8
otcova <otgercomas@gmail.com>
2022-12-24 21:26:12 +0100

    Delete vercel test

--- vercel/src/forceSsl.js
+++
@@ -1,7 +0,0 @@
-export const forceSsl = (request, response, next) => {
-	if (request.headers.host != "localhost:3000" &&
-		request.headers['x-forwarded-proto'] !== 'https') {
-		return response.redirect(['https://', request.get('Host'), request.url].join(''))
-	}
-	return next()
-}
